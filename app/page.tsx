4bbfc310a4561906c890bc1c800feefce0cfe06c
widski <dbakowsk@smail.uni-koeln.de>
2025-01-02 11:54:50 +0100

    update resume link

--- app/page.tsx
+++ app/page.tsx
@@ -116,7 +116,7 @@ export default function Home() {
               linkedin
             </a>
             <a
-              href="https://drive.google.com/file/d/1KbwyWDlwMjSE3PV9bUrZGOywsGtSI6xU/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Xnrz_BznFMANfttqRHSV-ysnjmlTeIAZ/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-500 dark:text-gray-400 hover:text-green-500 dark:hover:text-green-400 underline"
