4afa99b2de8481101f02b27eecba66e68731d0e9
widski <dbakowsk@smail.uni-koeln.de>
2025-01-02 08:48:30 +0100

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,32 @@ export const metadata: Metadata = {
   icons: {
     icon: '/images/favicon.ico',
   },
+  openGraph: {
+    type: 'website',
+    url: 'https://dawidbakowski.com',
+    title: 'Dawid Bakowski - Solutions Architect & Student',
+    description: 'Solutions Architect, Information Systems (CS & Business) Student.',
+    siteName: 'Dawid Bakowski',
+    images: [{
+      url: '/images/websitelogo.jpg',
+      width: 1200,
+      height: 630,
+      alt: 'Dawid Bakowski - Solutions Architect & Student'
+    }],
+    locale: 'en_US',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Dawid Bakowski - Solutions Architect & Student',
+    description: 'Solutions Architect, Information Systems (CS & Business) Student.',
+    images: ['/images/websitelogo.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  authors: [{ name: 'Dawid Bakowski' }],
+  publisher: 'Dawid Bakowski',
 }
 
 export default function RootLayout({
